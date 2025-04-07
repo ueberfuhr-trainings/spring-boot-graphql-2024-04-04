72ab0eb9bb2e0bfc7c748cb1c4eb3f6961ce6b8a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-04-07 12:12:06 +0200

    Fix GraphQl Test

--- src/test/java/com/samples/spring/GraphQlTests.java
+++ src/test/java/com/samples/spring/GraphQlTests.java
@@ -2,16 +2,16 @@ package com.samples.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.graphql.test.tester.GraphQlTester;
 
 @SpringBootTest
-@AutoConfigureHttpGraphQlTester
+@AutoConfigureGraphQlTester
 public class GraphQlTests {
 
   @Autowired
-  HttpGraphQlTester graphQlTester;
+  GraphQlTester graphQlTester;
   
   @Test
   void shouldReturnAllBlogPosts() throws Exception {
