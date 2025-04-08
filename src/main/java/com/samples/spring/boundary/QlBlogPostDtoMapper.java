e01f0d54db48221b30c0a4bded0d621f02ab8149
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2025-04-08 15:55:36 +0200

    Introduce MapStruct to generate mappers (#11)
    Co-authored-by: Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>

--- src/main/java/com/samples/spring/boundary/QlBlogPostDtoMapper.java
+++ src/main/java/com/samples/spring/boundary/QlBlogPostDtoMapper.java
@@ -1,45 +1,19 @@
 package com.samples.spring.boundary;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.samples.spring.domain.BlogPost;
 
-// TODO MapStruct
-@Component
-public class QlBlogPostDtoMapper {
+@Mapper(componentModel = "spring")
+public interface QlBlogPostDtoMapper {
 
-  public QlBlogPostDto map(BlogPost source) {
-    if(null == source) {
-      return null;
-    }
-    return new QlBlogPostDto(
-        source.getId(), 
-        source.getTitle(), 
-        source.getContent(), 
-        source.getCreated()
-    );
-  }
+  QlBlogPostDto map(BlogPost source);
 
-  public BlogPost map(QlBlogPostDto source) {
-    if(null == source) {
-      return null;
-    }
-    var result = new BlogPost();
-    result.setId(source.id());
-    result.setTitle(source.title());
-    result.setContent(source.content());
-    result.setCreated(source.created());
-    return result;
-  }
+  BlogPost map(QlBlogPostDto source);
 
-  public BlogPost map(QlBlogPostInputDto source) {
-    if(null == source) {
-      return null;
-    }
-    var result = new BlogPost();
-    result.setTitle(source.title());
-    result.setContent(source.content());
-    return result;
-  }
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "created", ignore = true)
+  BlogPost map(QlBlogPostInputDto source);
 
 }
