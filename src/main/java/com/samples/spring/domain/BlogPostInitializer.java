67a3373f8860e81657a6f26138fd9e70c98ba926
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2025-04-10 11:18:37 +0200

    Make initializer configurable, introduce profiles (#15)
    * Make initializer configurable, introduce profiles
    
    ---------
    
    Co-authored-by: Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>

--- src/main/java/com/samples/spring/domain/BlogPostInitializer.java
+++ src/main/java/com/samples/spring/domain/BlogPostInitializer.java
@@ -1,10 +1,17 @@
 package com.samples.spring.domain;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(
+    name = "application.initialization.enabled",
+    havingValue = "true",
+    matchIfMissing = true
+)
+// @Profile("local")
 public class BlogPostInitializer {
 
   private final BlogPostsService service;
