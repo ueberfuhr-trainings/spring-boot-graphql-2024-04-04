b5922a99788b01bf5008ab4c82213be535538aac
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-04-10 10:40:29 +0200

    Make initializer configurable, introduce profiles

--- src/test/java/com/samples/spring/domain/BlogPostInitializationTests.java
+++ src/test/java/com/samples/spring/domain/BlogPostInitializationTests.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "application.initialization.enabled=true"
+    }
+)
+// @ActiveProfiles("...")
 public class BlogPostInitializationTests {
 
   @Autowired
